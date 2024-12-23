8d53958521b89d9c01007d3eb2576827a43013af
vityapotapchuk <vityapotapchuk@gmail.com>
2024-12-23 17:41:24 -0500

    fix menu burger

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,32 +1,33 @@
+import { useState } from "react";
 import "./Navbar.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../../HURAGAN_logo.png";
 import { Link } from "react-scroll";
-import useForm from "../../hooks/useForm.js";
 
 function Navbar() {
-  const {isMobile, clickState, setMobile} = useForm()
+
+  const [ isMobile, setMobile ] = useState(false);
 
   return (
     <div className="navbar">
         <Link to="main" spy={true} smooth={false} offset={0}>
             <img className="logo-img-nav" src={logo} alt="logo" />
         </Link>
-        <nav className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile("main")}>
-          <Link style={{color: clickState === "projects" && "orange" }} to="projects" spy={true} smooth={false} offset={140} onClick={() => setMobile("projects")}>
+        <nav className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
+          <Link to="projects" spy={true} smooth={false} offset={140} onClick={isMobile ? () => setMobile(!isMobile) : null}>
             <p>Projects</p>
           </Link>
-          <Link style={{color: clickState === "about" && "orange" }} to="about" spy={true} smooth={false} offset={0} onClick={() => setMobile("about")}>
+          <Link to="about" spy={true} smooth={false} offset={0} onClick={isMobile ? () => setMobile(!isMobile) : null}>
             <p>About</p>
           </Link>
-          <Link style={{color: clickState === "services" && "orange" }} to="services" spy={true} smooth={false} offset={0} onClick={() => setMobile("services")}>
+          <Link to="services" spy={true} smooth={false} offset={0} onClick={isMobile ? () => setMobile(!isMobile) : null}>
             <p>Services</p>
           </Link>
-          <Link style={{color: clickState === "contacts" && "orange" }} to="contacts" spy={true} smooth={false} offset={0} onClick={() => setMobile("contacts")}>
+          <Link to="contacts" spy={true} smooth={false} offset={0} onClick={isMobile ? () => setMobile(!isMobile) : null}>
             <p>Contacts</p>
           </Link>
         </nav>
-        <button className="mobile-menu-icon" onClick={() => setMobile("mobile")} >
+        <button className="mobile-menu-icon" onClick={() => setMobile(!isMobile)} >
             {isMobile? <FaTimes/> : <FaBars />}
       </button>
       </div>
